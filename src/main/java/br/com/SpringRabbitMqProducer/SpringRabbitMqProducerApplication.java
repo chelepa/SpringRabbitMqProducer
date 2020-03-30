24e6ffbafced96df469f5fc60db6a62af5c109c3
Claudimir Chelepa <claudimir.chelepa@compasso.com.br>
2020-03-30 17:29:20 -0300

    First commit

---
+++ src/main/java/br/com/SpringRabbitMqProducer/SpringRabbitMqProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.SpringRabbitMqProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitMqProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitMqProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/SpringRabbitMqProducer/SpringRabbitMqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.SpringRabbitMqProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
